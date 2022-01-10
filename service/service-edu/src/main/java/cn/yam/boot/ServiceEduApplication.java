15104a0193f6d47a25f1cfbbffc534c8504ee72f
yuanmeng <ke197822@hotmail.com>
2022-01-10 11:19:48 +0800

    Initial commit

---
+++ service/service-edu/src/main/java/cn/yam/boot/ServiceEduApplication.java
@@ -0,0 +1,13 @@
+package cn.yam.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEduApplication.class, args);
+    }
+
+}
---
+++ service/service-edu/src/test/java/cn/yam/boot/ServiceEduApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yam.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
